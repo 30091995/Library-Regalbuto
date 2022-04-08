2ede97d72f699cd992b3c43a0e40cc44d00026c4
30091995 <francescosaccone95@live.it>
2022-04-08 09:55:12 +0200

    Fetch data for sigle book to prepopulate form

--- src/views/EmailRequest/index.js
+++ src/views/EmailRequest/index.js
@@ -1,7 +1,18 @@
 import { useParams } from "react-router-dom";
+import React from "react";
+import { client } from "../../client";
 
 export const EmailRequest = () => {
   const { bookId } = useParams();
+  const [book, setBook] = React.useState();
+
+  React.useEffect(() => {
+    async function fetchBook() {
+      const response = await client.getEntry(bookId);
+      setBook(response);
+    }
+    fetchBook();
+  }, []);
 
   return <>{bookId}</>;
 };
